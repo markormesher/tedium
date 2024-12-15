6be5d8b670900bec726623926814d0191c087097
Mark Ormesher <me@markormesher.co.uk>
2024-12-15 08:33:44 +0000

    print basic stats after running

--- internal/entrypoints/run.go
+++ internal/entrypoints/run.go
@@ -12,6 +12,16 @@ import (
 
 var l = logging.Logger
 
+type RunStats struct {
+	ReposDiscovered int
+	ReposSkipped    int
+	ReposFailed     int
+	JobsDiscovered  int
+	JobsFailed      int
+}
+
+var runStats = &RunStats{}
+
 func Run(conf *schema.TediumConfig) {
 	l.Info("Starting to gather chores to do")
 	jobQueue := gatherJobs(conf)
@@ -41,17 +51,19 @@ func Run(conf *schema.TediumConfig) {
 			break
 		}
 
+		runStats.JobsDiscovered++
+
 		platform, err := platforms.FromConfig(conf, job.PlatformConfig)
 		if err != nil {
 			l.Error("Failed to get platform for job - aborting this chore", "error", err, "repo", job.Repo.FullName(), "chore", job.Chore.Name)
-			// TODO: count - did some chores fail?
+			runStats.JobsFailed++
 			continue
 		}
 
 		err = executors.PrepareJob(platform, job)
 		if err != nil {
 			l.Error("Failed to prepare job - aborting this chore", "error", err, "repo", job.Repo.FullName(), "chore", job.Chore.Name)
-			// TODO: count - did some chores fail?
+			runStats.JobsFailed++
 			continue
 		}
 
@@ -59,10 +71,12 @@ func Run(conf *schema.TediumConfig) {
 		err = executor.ExecuteChore(job)
 		if err != nil {
 			l.Error("Error executing chore - aborting this chore", "error", err, "repo", job.Repo.FullName(), "chore", job.Chore.Name)
-			// TODO: count - did some chores fail?
+			runStats.JobsFailed++
 			continue
 		}
 	}
+
+	l.Info("Stats", "stats", runStats)
 
 	l.Info("Cleaning up executor")
 	err = executor.Deinit()
@@ -114,17 +128,20 @@ func gatherJobs(conf *schema.TediumConfig) *utils.Queue[schema.Job] {
 		}
 
 		l.Info("Finished discovering repos", "count", len(allRepos))
+		runStats.ReposDiscovered = len(allRepos)
 
 		for targetRepoIdx := range allRepos {
 			targetRepo := &allRepos[targetRepoIdx]
 
 			if targetRepo.Archived {
 				l.Info("Repo is archived - skipping", "repo", targetRepo.FullName())
+				runStats.ReposSkipped++
 				continue
 			}
 
 			if !platformConfig.AcceptsRepo(targetRepo.FullName()) {
 				l.Info("Repo does not match any filter - skipping", "repo", targetRepo.FullName())
+				runStats.ReposSkipped++
 				continue
 			}
 
@@ -135,13 +152,16 @@ func gatherJobs(conf *schema.TediumConfig) *utils.Queue[schema.Job] {
 			}
 			if !hasConfig {
 				l.Info("Repo has no Tedium config - skipping", "repo", targetRepo.FullName())
+				runStats.ReposSkipped++
 				continue
+
 				// TODO: auto-enrollment
 			}
 
 			repoConfig, err := resolveRepoConfig(conf, targetRepo)
 			if err != nil {
 				l.Error("Error resolving repo config", "repo", targetRepo.FullName(), "error", err)
+				runStats.ReposFailed++
 				os.Exit(1)
 			}
 
