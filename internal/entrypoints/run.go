6151c1f505ec3f45d36b040a37cf558654305a5b
Mark Ormesher <me@markormesher.co.uk>
2025-01-07 21:38:35 +0000

    run jobs in parallel

--- internal/entrypoints/run.go
+++ internal/entrypoints/run.go
@@ -2,6 +2,7 @@ package entrypoints
 
 import (
 	"os"
+	"sync"
 
 	"github.com/markormesher/tedium/internal/executors"
 	"github.com/markormesher/tedium/internal/logging"
@@ -23,15 +24,7 @@ type RunStats struct {
 var runStats = &RunStats{}
 
 func Run(conf *schema.TediumConfig) {
-	l.Info("Starting to gather chores to do")
-	jobQueue := gatherJobs(conf)
-	l.Info("Finished gathering chores to do", "count", jobQueue.Size)
-
-	if jobQueue.Size == 0 {
-		l.Info("No chores to do - exiting")
-		return
-	}
-
+	// setup the executor (this is cheap, it doesn't matter if we end up having no chores)
 	l.Info("Initialising executor")
 	executor, err := executors.FromExecutorConfig(&conf.Executor)
 	if err != nil {
@@ -45,43 +38,33 @@ func Run(conf *schema.TediumConfig) {
 		os.Exit(1)
 	}
 
-	for {
-		job, ok := jobQueue.Pop()
-		if !ok {
-			break
-		}
-
-		runStats.JobsDiscovered++
-
-		platform, err := platforms.FromConfig(conf, job.PlatformConfig)
-		if err != nil {
-			l.Error("Failed to get platform for job - aborting this chore", "error", err, "repo", job.Repo.FullName(), "chore", job.Chore.Name)
-			runStats.JobsFailed++
-			continue
-		}
-
-		err = executors.PrepareJob(platform, job)
-		if err != nil {
-			l.Error("Failed to prepare job - aborting this chore", "error", err, "repo", job.Repo.FullName(), "chore", job.Chore.Name)
-			runStats.JobsFailed++
-			continue
-		}
-
-		l.Info("Executing chore", "repo", job.Repo.FullName(), "chore", job.Chore.Name)
-		err = executor.ExecuteChore(job)
-		if err != nil {
-			l.Error("Error executing chore - aborting this chore", "error", err, "repo", job.Repo.FullName(), "chore", job.Chore.Name)
-			runStats.JobsFailed++
-			continue
-		}
+	// create job queue and worker pool
+	concurrency := 2
+	var workerWg sync.WaitGroup
+	jobQueue := make(chan schema.Job, 50)
+	for range concurrency {
+		workerWg.Add(1)
+		go func() {
+			for job := range jobQueue {
+				executeJob(conf, executor, job)
+			}
+			workerWg.Done()
+		}()
 	}
+
+	// gather jobs and feed them to the queue
+	l.Info("Starting to gather chores to do")
+	gatherJobs(conf, *&jobQueue)
+	close(jobQueue)
+	l.Info("Finished gathering chores to do")
+
+	// wait for our workers to finish handling all the jobs...
+	workerWg.Wait()
 
 	l.Info("Stats", "stats", runStats)
 }
 
-func gatherJobs(conf *schema.TediumConfig) *utils.Queue[schema.Job] {
-	var jobQueue utils.Queue[schema.Job]
-
+func gatherJobs(conf *schema.TediumConfig, jobQueue chan<- schema.Job) {
 	// init ALL platforms before trying to use ANY of them
 	for id := range conf.Platforms {
 		platformConfig := &conf.Platforms[id]
@@ -162,14 +145,14 @@ func gatherJobs(conf *schema.TediumConfig) *utils.Queue[schema.Job] {
 
 			for choreIdx := range repoConfig.Chores {
 				chore := repoConfig.Chores[choreIdx]
-				jobQueue.Push(schema.Job{
+				jobQueue <- schema.Job{
 					Config:          conf,
 					Repo:            targetRepo,
 					Chore:           chore,
 					PlatformConfig:  platformConfig,
 					WorkBranchName:  utils.UniqueName("work"),
 					FinalBranchName: utils.ConvertToBranchName(chore.Name),
-				})
+				}
 			}
 		}
 	}
@@ -191,6 +174,30 @@ func gatherJobs(conf *schema.TediumConfig) *utils.Queue[schema.Job] {
 			os.Exit(1)
 		}
 	}
+}
 
-	return &jobQueue
+func executeJob(conf *schema.TediumConfig, executor schema.Executor, job schema.Job) {
+	runStats.JobsDiscovered++
+
+	platform, err := platforms.FromConfig(conf, job.PlatformConfig)
+	if err != nil {
+		l.Error("Failed to get platform for job - aborting this chore", "error", err, "repo", job.Repo.FullName(), "chore", job.Chore.Name)
+		runStats.JobsFailed++
+		return
+	}
+
+	err = executors.PrepareJob(platform, &job)
+	if err != nil {
+		l.Error("Failed to prepare job - aborting this chore", "error", err, "repo", job.Repo.FullName(), "chore", job.Chore.Name)
+		runStats.JobsFailed++
+		return
+	}
+
+	l.Info("Executing chore", "repo", job.Repo.FullName(), "chore", job.Chore.Name)
+	err = executor.ExecuteChore(&job)
+	if err != nil {
+		l.Error("Error executing chore - aborting this chore", "error", err, "repo", job.Repo.FullName(), "chore", job.Chore.Name)
+		runStats.JobsFailed++
+		return
+	}
 }
