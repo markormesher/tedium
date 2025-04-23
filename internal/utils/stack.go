7af1209eece260dbba5ec40f46772baaa8b8ce86
Mark Ormesher <me@markormesher.co.uk>
2025-04-23 19:16:56 +0100

    feat: revamp config inheritance to do it properly

--- internal/utils/stack.go
+++ internal/utils/stack.go
@@ -1,23 +1,23 @@
 package utils
 
-type Queue[T any] struct {
+type Stack[T any] struct {
 	slice []T
 	Size  int
 }
 
-func (q *Queue[T]) Push(val T) {
+func (q *Stack[T]) Push(val T) {
 	q.slice = append(q.slice, val)
 	q.Size++
 }
 
-func (q *Queue[T]) Pop() (T, bool) {
+func (q *Stack[T]) Pop() (T, bool) {
 	if len(q.slice) == 0 {
 		var none T
 		return none, false
 	}
 
-	val := q.slice[0]
-	q.slice = q.slice[1:]
+	val := q.slice[len(q.slice)-1]
+	q.slice = q.slice[:len(q.slice)-1]
 
 	q.Size--
 
